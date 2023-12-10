c9452136b7aabf7798a5254c4cc488764986fb21
Kevin Glass <kglass@cafex.com>
2023-12-10 18:35:56 +0000

    Initial translations file check in

---
+++ copyin.sh
@@ -0,0 +1,2 @@
+cp ../taptapdungeon/src/assets/translations/* src/assets/translations
+cp ../taptapdungeon/quest-packs/default/translations/* quest-packs/default/translations
---
+++ copyout.sh
@@ -0,0 +1,2 @@
+cp src/assets/translations/* ../taptapdungeon/src/assets/translations
+cp quest-packs/default/translations/* ../taptapdungeon/quest-packs/default/translations
